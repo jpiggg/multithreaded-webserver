a1f02550a070a1e751535dec1d642e6f3a852f0e
gracheva <gracheva@tutu.tech>
2022-07-12 16:16:33 +0300

    added multuthreaded webserver

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::fs;
 
+use miltithreaded_webserver::ThreadPool;
+
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
+    
 
     stream.read(&mut buffer).unwrap();
 
@@ -30,9 +33,11 @@ fn handle_connection(mut stream: TcpStream) {
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
+    let pool = ThreadPool::new(4).unwrap();
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| handle_connection(stream));
     }
 }
